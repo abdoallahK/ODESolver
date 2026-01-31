208bdda568c18dd13833a5708995a82b58f56a7e
abdoallahK <abdoallah.k.2008@gmail.com>
2026-01-31 15:04:30 +0200

    Added Riemann sum
    Added to the integral class the Riemann sum method for integrals.

--- ODESolver/Integral.cs
+++ ODESolver/Integral.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,27 @@ namespace ODESolver
 {
     public static class Integral
     {
-        // Add The Riemann sum and Trapzoidal rule.
+        // Add Trapzoidal rule.
+
+        /// <summary>
+        /// Returns F(x) on [xstart, xend] using Riemann sum method.
+        /// </summary>
+        /// <param name="xstart">the initial x value.</param>
+        /// <param name="xend">the final x value.</param>
+        /// <param name="n">the number of steps.</param>
+        /// <param name="f">the explicit function to be integrated.</param>
+        /// <returns></returns>
+        public static double RiemannSum(double xstart, double xend, double n, Functions.ExplicitFunction f)
+        {
+            double dx = (xend - xstart) / n;
+            double x = xstart;
+            double sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                sum += dx * f(x);
+                x += dx;
+            }
+            return sum;
+        }
     }
 }
--- ODESolver/Program.cs
+++ ODESolver/Program.cs
@@ -9,9 +9,11 @@ namespace ODESolver
             double forward = Derivative.ForwardDerivative(1, 0.00001, f);
             double backward = Derivative.BackwardDerivative(1, 0.00001, f);
             double central = Derivative.CentralDerivative(1, 0.00001, f);
+            double riemann = Integral.RiemannSum(0, 1, 100000, f);
             Console.WriteLine(forward);
             Console.WriteLine(backward);
             Console.WriteLine(central);
+            Console.WriteLine(riemann);
         }
 
         public static double f(double x)
