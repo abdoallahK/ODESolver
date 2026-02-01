bbab73bf58613274cef215f8646d27a56428157b
abdoallahK <abdoallah.k.2008@gmail.com>
2026-02-02 00:15:35 +0200

    Added central difference for second order derivative.
    Added the central difference method for the second-order derivative to the Derivative class.

--- ODESolver/Derivative.cs
+++ ODESolver/Derivative.cs
@@ -38,17 +38,31 @@
 
 
         /// <summary>
-        /// Returns dy/dx at x = x0 for an explicit function using the central difference method.
+        /// Returns dy/dx at x = x0 for an explicit function using the 2nd central difference method.
         /// </summary>
         /// <param name="x0">The x value at which we want to find the derivative.</param>
-        /// <param name="h">the step size (the smaller the better)</param>
+        /// <param name="h">the step size (best is 10^-5)</param>
         /// <param name="f">the explicit function</param>
-        public static double CentralDerivative(double x0, double h, Functions.ExplicitFunction f)
+        public static double FirstOrderCentralDerivative(double x0, double h, Functions.ExplicitFunction f)
         {
             double addfxh = f(x0 + h);
             double subfxh = f(x0 - h);
             double change = addfxh - subfxh;
             double derivative = change / (2 * h);
+            return derivative;
+        }
+
+        /// <summary>
+        /// Returns d2y/dx2 at x = x0 for an explict function using 2nd order central difference method.
+        /// </summary>
+        /// <param name="x0">The x value at which we want to find the derivative.</param>
+        /// <param name="h">The step size.</param>
+        /// <param name="f">The explicit function.</param>
+        /// <returns></returns>
+        public static double SecondOrderCentralDerivative(double x0, double h, Functions.ExplicitFunction f)
+        {
+            double change = f(x0 + h) - ( 2 * f(x0)) + f(x0 - h);
+            double derivative = change / (h * h);
             return derivative;
         }
     }
--- ODESolver/Program.cs
+++ ODESolver/Program.cs
@@ -6,12 +6,14 @@
         {
             double forward = Derivative.ForwardDerivative(1, 0.00001, f);
             double backward = Derivative.BackwardDerivative(1, 0.00001, f);
-            double central = Derivative.CentralDerivative(1, 0.00001, f);
+            double firstCentral = Derivative.FirstOrderCentralDerivative(1, 0.00001, f);
+            double secondCentral = Derivative.SecondOrderCentralDerivative(1, 0.00001, f);
             double riemann = Integral.RiemannSum(0, 1, 100000, f);
             double trapezoidal = Integral.TrapezoidalRule(0, 1, 100000, f);
             Console.WriteLine(forward);
             Console.WriteLine(backward);
-            Console.WriteLine(central);
+            Console.WriteLine(firstCentral);
+            Console.WriteLine(secondCentral);
             Console.WriteLine(riemann);
             Console.WriteLine(trapezoidal);
         }
